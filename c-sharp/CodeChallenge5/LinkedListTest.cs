30cba54b90d45127bde23f5ebb3dd949662afca1
rgreen32 <ray.greenm@gmail.com>
2023-07-05 14:52:38 -0700

    tests

--- c-sharp/CodeChallenge5/LinkedListTest.cs
+++ c-sharp/CodeChallenge5/LinkedListTest.cs
@@ -6,14 +6,39 @@ namespace CodeChallengeTests
   public class CodeChallenge5Test
   {
     [Fact]
-    public void LinkedList_ContainsHeaderProperty()
+    public void LinkedList_Contains_HeaderProperty()
     {
       Type type = typeof(LinkedList);
 
-      PropertyInfo header = type.GetProperty("header");
-      PropertyInfo Header = type.GetProperty("Header");
+      PropertyInfo header = type.GetProperty("head");
+      PropertyInfo Header = type.GetProperty("Head");
 
       Assert.True(header != null || Header != null);
     }
+    
+    [Fact]
+    public void LinkedList_HeaderProperty_Is_NodeType()
+    {
+      Type type = typeof(LinkedList);
+
+      PropertyInfo header = type.GetProperty("head");
+
+      if(header == null){
+        header = type.GetProperty("Head");
+      }
+      Assert.Equal(header.PropertyType, typeof(Node));
+    }
+
+    [Fact]
+    public void LinkedList_Can_AddNewNode()
+    {
+      LinkedList list = new LinkedList();
+      Node node = new Node(5);
+
+      list.AddNewNode(node);
+      Assert.Equal(list.Head, node);
+
+    }
+
   }
 }
