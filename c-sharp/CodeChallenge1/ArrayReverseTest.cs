fa4c6dff3860ad73df21a03be52b3073d1b54f93
starlit-nox <kdkelly.1998@gmail.com>
2023-07-11 13:20:54 -0500

    Update ArrayReverseTest.cs

--- c-sharp/CodeChallenge1/ArrayReverseTest.cs
+++ c-sharp/CodeChallenge1/ArrayReverseTest.cs
@@ -3,46 +3,46 @@ using Xunit;
 
 namespace CodeChallengeTests
 {
-    public class CodeChallenge1Test
+  public class CodeChallenge1Test
+  {
+    [Fact]
+    public void ReverseArray_WithIntArray_ReturnsReversedArray()
     {
-        [Fact]
-        public void ReverseArray_WithIntArray_ReturnsReversedArray()
-        {
-            // Arrange
-            int[] inputArray = { 1, 2, 3, 4, 5 };
-            int[] expectedArray = { 5, 4, 3, 2, 1 };
+      // Arrange
+      int[] inputArray = { 1, 2, 3, 4, 5 };
+      int[] expectedArray = { 5, 4, 3, 2, 1 };
 
-            // Act
-            var result = CodeChallenge1.ReverseArray(inputArray);
+      // Act
+      var result = CodeChallenge1.ReverseArray(inputArray);
 
-            // Assert
-            Assert.Equal(expectedArray, result);
-        }
-
-        [Fact]
-        public void ReverseArray_WithEmptyArray_ReturnsSameArray()
-        {
-            // Arrange
-            int[] inputArray = new int[0];
-
-            // Act
-            var result = CodeChallenge1.ReverseArray(inputArray);
-
-            // Assert
-            Assert.Equal(inputArray, result);
-        }
-        [Fact]
-        public void ReverseArray_WithNegativeNumbers_ReturnsReversedArray()
-        {
-            // Arrange
-            int[] inputArray = { -3, -2, -1, 0, 1, 2, 3 };
-            int[] expectedArray = { 3, 2, 1, 0, -1, -2, -3 };
-
-            // Act
-            var result = CodeChallenge1.ReverseArray(inputArray);
-
-            // Assert
-            Assert.Equal(expectedArray, result);
-        }
+      // Assert
+      Assert.Equal(expectedArray, result);
     }
+
+    [Fact]
+    public void ReverseArray_WithEmptyArray_ReturnsSameArray()
+    {
+      // Arrange
+      int[] inputArray = new int[0];
+
+      // Act
+      var result = CodeChallenge1.ReverseArray(inputArray);
+
+      // Assert
+      Assert.Equal(inputArray, result);
+    }
+    [Fact]
+    public void ReverseArray_WithNegativeNumbers_ReturnsReversedArray()
+    {
+      // Arrange
+      int[] inputArray = { -3, -2, -1, 0, 1, 2, 3 };
+      int[] expectedArray = { 3, 2, 1, 0, -1, -2, -3 };
+
+      // Act
+      var result = CodeChallenge1.ReverseArray(inputArray);
+
+      // Assert
+      Assert.Equal(expectedArray, result);
+    }
+  }
 }
