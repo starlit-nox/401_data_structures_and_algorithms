98a9bb9e73b1170ef1d9d4b3458b957966e4ffaf
starlit-nox <kdkelly.1998@gmail.com>
2023-07-11 12:56:39 -0500

    Updated Array INsert

--- c-sharp/CodeChallenge2/ArrayInsertShiftTest.cs
+++ c-sharp/CodeChallenge2/ArrayInsertShiftTest.cs
@@ -1,65 +1,26 @@
-using CodeChallenges;
-using Xunit;
-
-namespace CodeChallengeTests
+namespace CodeChallenges
 {
-  public class CodeChallenge2Test
+  public class CodeChallenge2Modified : CodeChallenge2
   {
-    [Fact]
-    public void InsertShiftArray_With_OddNumber_Of_Elements()
+    public new static int[] InsertShiftArray(int[] array, int value)
     {
-      int[] inputArray = { 1, 2, 3, 4, 5, 6, 7 };
-      int inputValue = 9;
-      int[] expectedArray = { 1, 2, 3, 9, 7, 4, 5, 6 };
+      int middleIndex = array.Length / 2;
+      int[] newArray = new int[array.Length + 1];
 
-      int[] result = CodeChallenge2.InsertShiftArray(inputArray, inputValue);
+      for (int i = 0, j = 0; i < newArray.Length; i++, j++)
+      {
+        if (i == middleIndex)
+        {
+          newArray[i] = value;
+          j--;
+        }
+        else
+        {
+          newArray[i] = array[j];
+        }
+      }
 
-      Assert.Equal(result, expectedArray);
-    }
-    [Fact]
-    public void InsertShiftArray_With_EvenNumber_Of_Elements()
-    {
-      // Arrange
-      int[] inputArray = { 1, 2, 3, 4, 5, 6 };
-      int inputValue = 7;
-      int[] expectedArray = { 1, 2, 3, 7, 4, 5, 6 };
-
-      // Act
-      int[] result = CodeChallenge2.InsertShiftArray(inputArray, inputValue);
-
-      // Assert
-      Assert.Equal(expectedArray, result);
-    }
-
-    [Fact]
-    public void InsertShiftArray_With_EmptyArray()
-    {
-      // Arrange
-      int[] inputArray = new int[0];
-      int inputValue = 1;
-      int[] expectedArray = { 1 };
-
-      // Act
-      int[] result = CodeChallenge2.InsertShiftArray(inputArray, inputValue);
-
-      // Assert
-      Assert.Equal(expectedArray, result);
-    }
-
-
-    [Fact]
-    public void InsertShiftArray_With_NegativeNumbers()
-    {
-      // Arrange
-      int[] inputArray = { -3, -2, -1, 0, 1 };
-      int inputValue = -4;
-      int[] expectedArray = { -3, -2, -4, -1, 0, 1 };
-
-      // Act
-      int[] result = CodeChallenge2.InsertShiftArray(inputArray, inputValue);
-
-      // Assert
-      Assert.Equal(expectedArray, result);
+      return newArray;
     }
   }
 }
