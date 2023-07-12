6776083ed9915151ba9cf5bab6376a80e6a30da7
starlit-nox <kdkelly.1998@gmail.com>
2023-07-12 07:54:21 -0500

    array-binary-search.

--- c-sharp/CodeChallenge3/ArrayBinarySearch.cs
+++ c-sharp/CodeChallenge3/ArrayBinarySearch.cs
@@ -1,11 +1,36 @@
-namespace CodeChallenges 
+namespace CodeChallenges
 {
-    public class CodeChallenge3
+  public class CodeChallenge3
+  {
+    public static int BinarySearch(int[] array, int key)
     {
-        public static int BinarySearch(int[] array, int key)
+      int left = 0;
+      int right = array.Length - 1;
+
+      while (left <= right)
+      {
+        int mid = left + (right - left) / 2;
+
+        if (array[mid] == key)
         {
-            int foundElementIndex = -1;
-            return foundElementIndex;
+          while (mid > 0 && array[mid - 1] == key)
+          {
+            mid--;
+          }
+          return mid;
         }
+
+        if (array[mid] < key)
+        {
+          left = mid + 1;
+        }
+        else
+        {
+          right = mid - 1;
+        }
+      }
+
+      return -1;
     }
+  }
 }
