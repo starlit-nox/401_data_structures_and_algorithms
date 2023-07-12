49b0d3a8b5ae4cc1ec7570efa001fd967d912216
rgreen32 <ray.greenm@gmail.com>
2023-07-12 14:16:12 -0700

    added codechallenge4

---
+++ c-sharp/CodeChallenge4/FibonacciAndRowSums.cs
@@ -0,0 +1,19 @@
+namespace CodeChallenges 
+{
+    public class CodeChallenge4
+    {
+        public static int GetNthNumberInFibonacciSequence(int nthNumberInSequence)
+        {
+            int result = 0;
+
+            return result;
+        }
+
+        public static int[] RowSums(int[][] matrix)
+        {
+            int[] rowSums = new int[matrix[0].Length];
+            return rowSums;
+        }
+
+    }
+}
