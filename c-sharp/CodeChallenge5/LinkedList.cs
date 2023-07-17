d0dd58acf68a3d5551a27962ca3cd5cf0a8f8428
rgreen32 <ray.greenm@gmail.com>
2023-07-17 08:16:05 -0700

    challenge 6 test

--- c-sharp/CodeChallenge5/LinkedList.cs
+++ c-sharp/CodeChallenge5/LinkedList.cs
@@ -2,7 +2,7 @@ namespace CodeChallenges
 {
     public class LinkedList
     {
-        public Node Head {get; set;}
+        // public Node Head {get; set;}
 
     }
 
--- c-sharp/CodeChallenge6/LinkedListInsertTest.cs
+++ c-sharp/CodeChallenge6/LinkedListInsertTest.cs
@@ -6,19 +6,25 @@ namespace CodeChallengeTests
 {
   public class CodeChallenge6Test
   {
+
     [Fact]
     public void InsertableLinkedList_Can_Append_New_Value()
     {
-      InsertableLinkedList list = new InsertableLinkedList();
-      MethodInfo appendMethod = typeof(LinkedList).GetMethod("Append");
-      Assert.True(appendMethod != null);
-      
-      int valueToInsert = 10;
-      
-      list.Append(valueToInsert);
-      appendMethod.Invoke(list, new object[] {valueToInsert});
-       
-      Assert.Equal(true, false);
+        InsertableLinkedList list = new InsertableLinkedList();
+
+        MethodInfo appendMethod = typeof(InsertableLinkedList).GetMethod("Append");
+        Assert.NotNull(appendMethod);
+
+        int valueToInsert = 10;
+
+        appendMethod.Invoke(list, new object[] { valueToInsert });
+        PropertyInfo head = typeof(LinkedList).GetProperty("Head");
+
+        Node headValue = (Node)head.GetValue(list);
+        PropertyInfo ValueProperty = typeof(Node).GetProperty("Value");
+        int result = (int)ValueProperty.GetValue(headValue);
+        Assert.Equal(valueToInsert, result);
     }
+
   }
 }
