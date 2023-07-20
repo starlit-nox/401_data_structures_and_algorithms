cdd229ecf03bf22eec1c54abea3fcf4cb6f5365f
rgreen32 <ray.greenm@gmail.com>
2023-07-20 09:10:23 -0700

    CodeChallenge8

--- c-sharp/CodeChallenge8/LinkedListZip.cs
+++ c-sharp/CodeChallenge8/LinkedListZip.cs
@@ -3,28 +3,10 @@ namespace CodeChallenges
 {
   public class CodeChallenge8
   {
-    public static LinkedList<int> ZipLists(LinkedList<int> list1, LinkedList<int> list2)
+    public static LinkedListKth ZipLists(LinkedListKth list1, LinkedListKth list2)
     {
+      LinkedListKth result = new LinkedListKth();
 
-      LinkedList<int> result = new LinkedList<int>();
-
-      LinkedListNode<int> current1 = list1.First;
-      LinkedListNode<int> current2 = list2.First;
-
-      while (current1 != null || current2 != null)
-      {
-        if (current1 != null)
-        {
-          result.AddLast(current1.Value);
-          current1 = current1.Next;
-        }
-
-        if (current2 != null)
-        {
-          result.AddLast(current2.Value);
-          current2 = current2.Next;
-        }
-      }
 
       return result;
     }
--- c-sharp/CodeChallenge8/LinkedListZipTest.cs
+++ c-sharp/CodeChallenge8/LinkedListZipTest.cs
@@ -1,5 +1,4 @@
 using CodeChallenges;
-using System.Collections.Generic;
 using Xunit;
 
 namespace CodeChallengeTests
@@ -9,63 +8,64 @@ namespace CodeChallengeTests
     [Fact]
     public void LinkedList_ZipLists_Both_Lists_Same_Length()
     {
-        LinkedList<int> list1 = new LinkedList<int>();
-        list1.AddLast(1);
-        list1.AddLast(2);
-        list1.AddLast(3);
+        LinkedListKth list1 = new LinkedListKth();
+        list1.Insert(3);
+        list1.Insert(2);
+        list1.Insert(1);
 
-        LinkedList<int> list2 = new LinkedList<int>();
-        list2.AddLast(10);
-        list2.AddLast(20);
-        list2.AddLast(30);
 
-        LinkedList<int> result = CodeChallenge8.ZipLists(list1, list2);
+        LinkedListKth list2 = new LinkedListKth();
+        list2.Insert(30);
+        list2.Insert(20);
+        list2.Insert(10);
 
-        Assert.Equal(1, result.First.Value); // First node
-        Assert.Equal(10, result.First.Next.Value); // Second node
-        Assert.Equal(2, result.First.Next.Next.Value); // Third node
-        Assert.Equal(20, result.First.Next.Next.Next.Value); // Fourth node
-        Assert.Equal(3, result.First.Next.Next.Next.Next.Value); // Fifth node
-        Assert.Equal(30, result.First.Next.Next.Next.Next.Next.Value); // Sixth node
+        LinkedListKth result = CodeChallenge8.ZipLists(list1, list2);
+
+        Assert.Equal(1, result.KthFromEnd(6)); // First node from the end
+        Assert.Equal(10, result.KthFromEnd(5)); // Second node from the end
+        Assert.Equal(2, result.KthFromEnd(4)); // Third node from the end
+        Assert.Equal(20, result.KthFromEnd(3)); // Fourth node from the end
+        Assert.Equal(3, result.KthFromEnd(2)); // Fifth node from the end
+        Assert.Equal(30, result.KthFromEnd(1)); // Sixth node from the end
     }
 
     [Fact]
     public void LinkedList_ZipLists_First_List_Longer()
     {
-        LinkedList<int> list1 = new LinkedList<int>();
-        list1.AddLast(1);
-        list1.AddLast(2);
-        list1.AddLast(3);
-        list1.AddLast(4);
+        LinkedListKth list1 = new LinkedListKth();
+        list1.Insert(4);
+        list1.Insert(3);
+        list1.Insert(2);
+        list1.Insert(1);
 
-        LinkedList<int> list2 = new LinkedList<int>();
-        list2.AddLast(10);
-        list2.AddLast(20);
+        LinkedListKth list2 = new LinkedListKth();
+        list2.Insert(20);
+        list2.Insert(10);
 
-        LinkedList<int> result = CodeChallenge8.ZipLists(list1, list2);
+        LinkedListKth result = CodeChallenge8.ZipLists(list1, list2);
 
-        Assert.Equal(1, result.First.Value);
-        Assert.Equal(10, result.First.Next.Value);
-        Assert.Equal(2, result.First.Next.Next.Value);
-        Assert.Equal(20, result.First.Next.Next.Next.Value);
-        Assert.Equal(3, result.First.Next.Next.Next.Next.Value);
-        Assert.Equal(4, result.First.Next.Next.Next.Next.Next.Value);
+        Assert.Equal(1, result.KthFromEnd(6));
+        Assert.Equal(10, result.KthFromEnd(5));
+        Assert.Equal(2, result.KthFromEnd(4));
+        Assert.Equal(20, result.KthFromEnd(3));
+        Assert.Equal(3, result.KthFromEnd(2));
+        Assert.Equal(4, result.KthFromEnd(1));
     }
 
     [Fact]
     public void LinkedList_ZipLists_Second_List_Longer()
     {
-        LinkedList<int> list1 = new LinkedList<int>();
-        list1.AddLast(1);
-        list1.AddLast(2);
+        LinkedListKth list1 = new LinkedListKth();
+        list1.Insert(2);
+        list1.Insert(1);
 
-        LinkedList list2 = new LinkedList();
-        list2.Insert(10);
-        list2.Insert(20);
-        list2.Insert(30);
+        LinkedListKth list2 = new LinkedListKth();
         list2.Insert(40);
+        list2.Insert(30);
+        list2.Insert(20);
+        list2.Insert(10);
 
-        LinkedList result = LinkedList.ZipLists(list1, list2);
+        LinkedListKth result = CodeChallenge8.ZipLists(list1, list2);
 
         Assert.Equal(1, result.KthFromEnd(6));
         Assert.Equal(10, result.KthFromEnd(5));
@@ -75,30 +75,30 @@ namespace CodeChallengeTests
         Assert.Equal(40, result.KthFromEnd(1));
     }
 
-    // [Fact]
-    // public void LinkedList_ZipLists_One_List_Empty()
-    // {
-    //     LinkedList list1 = new LinkedList();
-    //     list1.Insert(1);
-    //     list1.Insert(2);
+    [Fact]
+    public void LinkedList_ZipLists_One_List_Empty()
+    {
+        LinkedListKth list1 = new LinkedListKth();
+        list1.Insert(2);
+        list1.Insert(1);
 
-    //     LinkedList list2 = new LinkedList();
+        LinkedListKth list2 = new LinkedListKth();
 
-    //     LinkedList result = LinkedList.ZipLists(list1, list2);
+        LinkedListKth result = CodeChallenge8.ZipLists(list1, list2);
 
-    //     Assert.Equal(1, result.KthFromEnd(2));
-    //     Assert.Equal(2, result.KthFromEnd(1));
-    // }
+        Assert.Equal(1, result.KthFromEnd(2));
+        Assert.Equal(2, result.KthFromEnd(1));
+    }
 
-    // [Fact]
-    // public void LinkedList_ZipLists_Both_Lists_Empty()
-    // {
-    //     LinkedList list1 = new LinkedList();
-    //     LinkedList list2 = new LinkedList();
+    [Fact]
+    public void LinkedList_ZipLists_Both_Lists_Empty()
+    {
+        LinkedListKth list1 = new LinkedListKth();
+        LinkedListKth list2 = new LinkedListKth();
 
-    //     LinkedList result = LinkedList.ZipLists(list1, list2);
+        LinkedListKth result = CodeChallenge8.ZipLists(list1, list2);
 
-    //     Assert.Null(result.head);
-    // }
+        Assert.Null(result.Head);
+    }
 }
 }
