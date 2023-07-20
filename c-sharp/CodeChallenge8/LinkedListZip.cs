352ceebdc20630805bd040fa216713f5e1498df5
rgreen32 <ray.greenm@gmail.com>
2023-07-20 12:44:59 -0700

    removed implementation details

--- c-sharp/CodeChallenge8/LinkedListZip.cs
+++ c-sharp/CodeChallenge8/LinkedListZip.cs
@@ -6,42 +6,7 @@ namespace CodeChallenges
     public static LinkedListKth ZipLists(LinkedListKth list1, LinkedListKth list2)
     {
       LinkedListKth result = new LinkedListKth();
-
-      Node current1 = list1.Head;
-      Node current2 = list2.Head;
-
-      Node resultCurrent = null;
-
-      while (current1 != null || current2 != null)
-      {
-        if (current1 != null)
-        {
-          resultCurrent = AppendNodeToResult(result, resultCurrent, current1.Value);
-          current1 = current1.Next;
-        }
-
-        if (current2 != null)
-        {
-          resultCurrent = AppendNodeToResult(result, resultCurrent, current2.Value);
-          current2 = current2.Next;
-        }
-      }
-
       return result;
-    }
-    private static Node AppendNodeToResult(LinkedList result, Node resultCurrent, int value)
-    {
-        Node newNode = new Node(value);
-        if (result.Head == null)
-        {
-            result.Head = newNode;
-        }
-        else
-        {
-            resultCurrent.Next = newNode;
-        }
-
-        return newNode;
     }
   }
 }
