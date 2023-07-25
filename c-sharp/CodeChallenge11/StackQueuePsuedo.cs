ec677a6344c5b371fcfab27b6f22c6914340c266
rgreen32 <ray.greenm@gmail.com>
2023-07-25 16:20:41 -0700

    edit

--- c-sharp/CodeChallenge11/StackQueuePsuedo.cs
+++ c-sharp/CodeChallenge11/StackQueuePsuedo.cs
@@ -1,15 +1,29 @@
-namespace StackQueuePsuedos 
+namespace CodeChallenges 
 {
     public class StackQueuePsuedo
     {
+        private Stack stack1 = new CodeChallenges.Stack();
+        private Stack stack2 = new Stack();
+
         public void Enqueue(int value)
         {
-            
+            while (stack1.Count > 0)
+            {
+                stack2.Push(stack1.Pop());
+            }
+            stack1.Push(value);
+            while (stack2.Count > 0)
+            {
+                stack1.Push(stack2.Pop());
+            }
         }
 
         public int Dequeue()
         {
-            return 0;
+            if (stack1.Count == 0)
+                throw new InvalidOperationException("Queue is empty.");
+
+            return stack1.Pop();
         }
     }
 }
--- c-sharp/CodeChallenge11/StackQueuePsuedoTest.cs
+++ c-sharp/CodeChallenge11/StackQueuePsuedoTest.cs
@@ -1,54 +1,79 @@
-using StackQueuePsuedos;
+using System.Reflection;
 using Xunit;
+using CodeChallenges;
 
 namespace CodeChallengeTests
 {
-  public class CodeChallenge11Tests
-  {
-    [Fact]
-    public void Enqueue_WhenCalled_AddsItemToThePseudoQueue()
+    public class CodeChallenge11Tests
     {
-      // Arrange
-      var pseudoQueue = new StackQueuePsuedo();
+        private Stack<int> GetEnqueueStack(StackQueuePsuedo pseudoQueue)
+        {
+            var enqueueStackField = typeof(StackQueuePsuedo).GetField("enqueueStack", BindingFlags.Instance | BindingFlags.NonPublic);
+            return (Stack<int>)enqueueStackField.GetValue(pseudoQueue);
+        }
 
-      // Act
-      pseudoQueue.Enqueue(1);
-      pseudoQueue.Enqueue(2);
-      pseudoQueue.Enqueue(3);
+        private Stack<int> GetDequeueStack(StackQueuePsuedo pseudoQueue)
+        {
+            var dequeueStackField = typeof(StackQueuePsuedo).GetField("dequeueStack", BindingFlags.Instance | BindingFlags.NonPublic);
+            return (Stack<int>)dequeueStackField.GetValue(pseudoQueue);
+        }
 
-      // Assert
-      Assert.Equal(1, pseudoQueue.Dequeue());
-      Assert.Equal(2, pseudoQueue.Dequeue());
-      Assert.Equal(3, pseudoQueue.Dequeue());
+        [Fact]
+        public void Enqueue_WhenCalled_AddsItemToThePseudoQueue()
+        {
+            // Arrange
+            var pseudoQueue = new StackQueuePsuedo();
+
+            // Act
+            pseudoQueue.Enqueue(1);
+            pseudoQueue.Enqueue(2);
+            pseudoQueue.Enqueue(3);
+
+            // Assert
+            var enqueueStack = GetEnqueueStack(pseudoQueue);
+            Assert.Equal(3, enqueueStack.Count);
+
+            var dequeueStack = GetDequeueStack(pseudoQueue);
+            Assert.Equal(0, dequeueStack.Count);
+
+            Assert.Equal(1, pseudoQueue.Dequeue());
+            Assert.Equal(2, pseudoQueue.Dequeue());
+            Assert.Equal(3, pseudoQueue.Dequeue());
+        }
+
+        [Fact]
+        public void Dequeue_EmptyQueue_ThrowsInvalidOperationException()
+        {
+            // Arrange
+            var pseudoQueue = new StackQueuePsuedo();
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => pseudoQueue.Dequeue());
+        }
+
+        [Fact]
+        public void EnqueueAndDequeue_InterleavedOperations_ExpectedOrder()
+        {
+            // Arrange
+            var pseudoQueue = new StackQueuePsuedo();
+
+            // Act
+            pseudoQueue.Enqueue(11);
+            pseudoQueue.Enqueue(12);
+            var firstDequeue = pseudoQueue.Dequeue();
+            pseudoQueue.Enqueue(13);
+            var secondDequeue = pseudoQueue.Dequeue();
+
+            // Assert
+            var enqueueStack = GetEnqueueStack(pseudoQueue);
+            Assert.Equal(1, enqueueStack.Count);
+
+            var dequeueStack = GetDequeueStack(pseudoQueue);
+            Assert.Equal(1, dequeueStack.Count);
+
+            Assert.Equal(11, firstDequeue);
+            Assert.Equal(12, secondDequeue);
+            Assert.Equal(13, pseudoQueue.Dequeue());
+        }
     }
-
-    [Fact]
-    public void Dequeue_EmptyQueue_ThrowsInvalidOperationException()
-    {
-      // Arrange
-      var pseudoQueue = new StackQueuePsuedo();
-
-      // Act & Assert
-      Assert.Throws<InvalidOperationException>(() => pseudoQueue.Dequeue());
-    }
-
-    [Fact]
-    public void EnqueueAndDequeue_InterleavedOperations_ExpectedOrder()
-    {
-      // Arrange
-      var pseudoQueue = new StackQueuePsuedo();
-
-      // Act
-      pseudoQueue.Enqueue(11);
-      pseudoQueue.Enqueue(12);
-      var firstDequeue = pseudoQueue.Dequeue();
-      pseudoQueue.Enqueue(13);
-      var secondDequeue = pseudoQueue.Dequeue();
-
-      // Assert
-      Assert.Equal(11, firstDequeue);
-      Assert.Equal(12, secondDequeue);
-      Assert.Equal(13, pseudoQueue.Dequeue());
-    }
-  }
 }
