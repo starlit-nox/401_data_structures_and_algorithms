e597b89089d7cb6c8a999a3a9aef0166b44b7ffa
rgreen32 <ray.greenm@gmail.com>
2023-07-27 09:32:53 -0700

    cc12

--- c-sharp/CodeChallenge12/StackAndQueueAnimalShelter.cs
+++ c-sharp/CodeChallenge12/StackAndQueueAnimalShelter.cs
@@ -1,8 +1,20 @@
 namespace StackAndQueueAnimalShelters 
 {
+
+    public class Animal
+    {
+        
+    }
+
+
     public class StackAndQueueAnimalShelter
     {
-        public void Enqueue()
+        public void Enqueue(Animal animal)
+        {
+            
+        }
+
+        public Animal Dequeue(string pref)
         {
             
         }
