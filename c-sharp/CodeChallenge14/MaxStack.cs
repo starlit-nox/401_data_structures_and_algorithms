30b9ae6694a59bedf6cad6b7ceb9cec9a41c60d2
rgreen32 <ray.greenm@gmail.com>
2023-07-30 21:26:51 -0700

    cc14

--- c-sharp/CodeChallenge14/MaxStack.cs
+++ c-sharp/CodeChallenge14/MaxStack.cs
@@ -1,9 +1,19 @@
 using System;
 
-namespace CodeChallenges 
+namespace CodeChallenges
 {
+  public class CodeChallenge14
+  {
     public class MaxStack
     {
 
     }
+
+    public string DuckDuckGoose(string[] stringArray, int k)
+    {
+        return "";
+    }
+  }
+
+
 }
