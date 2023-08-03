9300d52dbb944d2d4dfdf936b5fb100a99682897
starlit-nox <kdkelly.1998@gmail.com>
2023-08-03 09:18:59 -0500

    Test Update
    Had to update the test to match Ray's new test

--- c-sharp/CodeChallenge7/LinkedListKth.cs
+++ c-sharp/CodeChallenge7/LinkedListKth.cs
@@ -1,3 +1,37 @@
+//namespace CodeChallenges
+//{
+//  public class LinkedListKth : InsertableLinkedList
+//  {
+//    public int KthFromEnd(int kthFromEnd)
+//    {
+//      if (kthFromEnd < 0)
+//      {
+//        throw new ArgumentException("Must be positive.");
+//      }
+
+//      Node fast = Head;
+//      Node slow = Head;
+
+//      for (int i = 0; i < kthFromEnd; i++)
+//      {
+//        if (fast == null)
+//        {
+//          throw new ArgumentException("Its too big, make it smaller.");
+//        }
+//        fast = fast.Next;
+//      }
+
+//      while (fast != null)
+//      {
+//        fast = fast.Next;
+//        slow = slow.Next;
+//      }
+
+//      return slow.Value;
+//    }
+//  }
+//}
+
 namespace CodeChallenges
 {
   public class LinkedListKth : InsertableLinkedList
