4fec087ad9a9c338f3a78a0affda8664f3dc2877
rgreen32 <ray.greenm@gmail.com>
2023-08-03 12:37:52 -0700

    cc14

--- c-sharp/CodeChallenge14/CodeChallenge14.cs
+++ c-sharp/CodeChallenge14/CodeChallenge14.cs
@@ -11,29 +11,7 @@ namespace CodeChallenges
 
     public static string DuckDuckGoose(string[] stringArray, int k)
     {
-      if (stringArray == null || stringArray.Length == 0)
-        throw new ArgumentException("The array must not be null or empty.");
-
-      if (k <= 0)
-        throw new ArgumentException("The value of k must be greater than zero.");
-
-      int currentIndex = 0;
-
-      while (stringArray.Length > 1)
-      {
-        currentIndex = (currentIndex + k - 1) % stringArray.Length;
-        stringArray = RemoveAtIndex(stringArray, currentIndex);
-      }
-
-      return stringArray[0];
-    }
-
-    private static string[] RemoveAtIndex(string[] array, int index)
-    {
-      string[] newArray = new string[array.Length - 1];
-      Array.Copy(array, 0, newArray, 0, index);
-      Array.Copy(array, index + 1, newArray, index, array.Length - index - 1);
-      return newArray;
+      return "";
     }
   }
 }
