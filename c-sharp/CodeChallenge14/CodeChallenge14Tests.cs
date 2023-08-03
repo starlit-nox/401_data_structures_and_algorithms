2f1c182897e3825368c04d25595636ecf56b9250
rgreen32 <ray.greenm@gmail.com>
2023-08-03 12:36:48 -0700

    cc14

--- c-sharp/CodeChallenge14/CodeChallenge14Tests.cs
+++ c-sharp/CodeChallenge14/CodeChallenge14Tests.cs
@@ -7,13 +7,6 @@ namespace CodeChallengeTests
   public class CodeChallenge14Tests
   {
 
-    private object GetPrivateField(object instance, string fieldName)
-    {
-      Type type = instance.GetType();
-      FieldInfo fieldInfo = type.GetField(fieldName);
-      return fieldInfo.GetValue(instance);
-    }
-
     private object InvokePrivateMethod(object instance, string methodName, params object[] parameters)
     {
       Type type = instance.GetType();
@@ -34,8 +27,6 @@ namespace CodeChallengeTests
       InvokePrivateMethod(maxStack, "Push", 9);
 
       // Use GetPrivateField to get the values of private fields (stack, maxStack, and top)
-
-
       Assert.Equal(9, (int)InvokePrivateMethod(maxStack, "GetMax"));
 
       InvokePrivateMethod(maxStack, "Pop");
@@ -46,6 +37,69 @@ namespace CodeChallengeTests
 
       InvokePrivateMethod(maxStack, "Push", 10);
       Assert.Equal(10, (int)InvokePrivateMethod(maxStack, "GetMax"));
+    }
+    [Fact]
+    public void DuckDuckGoose_OnePerson_ShouldReturnTheOnlyPerson()
+    {
+      // Arrange
+      string[] stringArray = new string[] { "John" };
+      int k = 2;
+
+      // Act
+      string result = CodeChallenge14.DuckDuckGoose(stringArray, k);
+
+      // Assert
+      Assert.Equal("John", result);
+    }
+
+    [Fact]
+    public void DuckDuckGoose_MultiplePeople_ShouldReturnLastPerson()
+    {
+      // Arrange
+      string[] stringArray = new string[] { "Alice", "Bob", "Charlie", "David", "Eve" };
+      int k = 3;
+
+      // Act
+      string result = CodeChallenge14.DuckDuckGoose(stringArray, k);
+
+      // Assert
+      Assert.Equal("David", result);
+    }
+
+    [Fact]
+    public void DuckDuckGoose_EmptyArray_ShouldThrowArgumentException()
+    {
+      // Arrange
+      string[] stringArray = new string[] { };
+      int k = 3;
+
+      // Act & Assert
+      Assert.Throws<ArgumentException>(() => CodeChallenge14.DuckDuckGoose(stringArray, k));
+    }
+
+    [Fact]
+    public void DuckDuckGoose_NegativeKValue_ShouldThrowArgumentException()
+    {
+      // Arrange
+      string[] stringArray = new string[] { "Alice", "Bob", "Charlie", "David", "Eve" };
+      int k = -2;
+
+      // Act & Assert
+      Assert.Throws<ArgumentException>(() => CodeChallenge14.DuckDuckGoose(stringArray, k));
+    }
+
+    [Fact]
+    public void DuckDuckGoose_KGreaterThanArrayLength_ShouldWrapAround()
+    {
+      // Arrange
+      string[] stringArray = new string[] {  "Charlie" };
+      int k = 10; // k is greater than the array length
+
+      // Act
+      string result = CodeChallenge14.DuckDuckGoose(stringArray, k);
+
+      // Assert
+      Assert.Equal("Charlie", result);
     }
   }
 }
