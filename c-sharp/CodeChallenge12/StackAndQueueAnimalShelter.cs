41f79e6bcd5d19138e3a79719c176b249e68b53b
starlit-nox <kdkelly.1998@gmail.com>
2023-08-10 14:18:02 -0500

    Update StackAndQueueAnimalShelter.cs

--- c-sharp/CodeChallenge12/StackAndQueueAnimalShelter.cs
+++ c-sharp/CodeChallenge12/StackAndQueueAnimalShelter.cs
@@ -1,22 +1,58 @@
 namespace CodeChallenges
 {
-
-  public class Animal
-  {
-
-  }
-
-
-  public class StackAndQueueAnimalShelter
-  {
-    public void Enqueue(Animal animal)
+    public class Animal
     {
+        public string Species { get; set; }
+        public string Name { get; set; }
 
+        public Animal(string species, string name)
+        {
+            Species = species;
+            Name = name;
+        }
     }
 
-    // public Animal Dequeue(string pref)
-    // {
+    public class StackAndQueueAnimalShelter
+    {
+        private Queue<Animal> dogsQueue;
+        private Queue<Animal> catsQueue;
 
-    // }
-  }
+        public StackAndQueueAnimalShelter()
+        {
+            dogsQueue = new Queue<Animal>();
+            catsQueue = new Queue<Animal>();
+        }
+
+        public void Enqueue(Animal animal)
+        {
+            if (animal.Species == "dog")
+            {
+                dogsQueue.Enqueue(animal);
+            }
+            else if (animal.Species == "cat")
+            {
+                catsQueue.Enqueue(animal);
+            }
+        }
+
+        public Animal Dequeue(string pref)
+        {
+            if (pref == "dog")
+            {
+                if (dogsQueue.Count > 0)
+                {
+                    return dogsQueue.Dequeue();
+                }
+            }
+            else if (pref == "cat")
+            {
+                if (catsQueue.Count > 0)
+                {
+                    return catsQueue.Dequeue();
+                }
+            }
+
+            return null;
+        }
+    }
 }
